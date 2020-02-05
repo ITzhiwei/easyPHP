963da2298a7825953a5e9e5d18a1ab3d2bde89ee
ITwuzhiwei <2394599321@qq.com>
2020-02-06 00:11:44 +0800

    入口升级，有默认入口，可以配置域名入口

--- application/index/controller/index2.php
+++ application/index/controller/index2.php
@@ -7,7 +7,7 @@ class index2 extends controller{
     
     
     public function index(){
-        $row = Db::table('article')->where('id', 1)->select();
+       // $row = Db::table('article')->where('id', 1)->select();
        // var_dump($row);
         echo 'hello2';
     }
--- config/route.php
+++ config/route.php
@@ -5,12 +5,25 @@
 
 return [
     // URL伪静态后缀，一维数组
-    'urlHtmlSuffix'       => ['html', 'htm'],
-    //路由自定义  例子： 'urlHtmlCustomize'      => ['indes/:id'=>'index/index/index2/index', 'uinfo'=>'index/index/user/info'],
-    'urlHtmlCustomize'      => [],
-    //自动路由多入口白名单；
+    'urlHtmlSuffix'         => ['html', 'htm'],
+    //路由自定义  例子： 'urlHtmlCustomize'      => [ '*' => ['indes/:id'=>'index/index/index2/index', 'uinfo'=>'index/index/user/info'] ],
+    'urlHtmlCustomize'      => [
+        //所有域名
+        '*'=>[],
+        //指定域名，如果指定域名与所有域名存在冲突，则自动以指定域名的为准（指定域名的设置优先）
+        'www.wei.com'=>['indes/:id'=>'index/index/index']
+    ],
+    //自动路由一级目录入口白名单；
     'door'                  => [
                                 'index'=>__DIR__.'/../application/',
-                                'app2'=>__DIR__.'/../application2/',
+                                'application2'=>__DIR__.'/../application2/',
                                ],
+    //域名指定一目录入口
+    'urlEntranceData'       => [
+        //所有域名默认
+        '*'=>'index',
+        //指定入口
+        'www.weia.com'=>'application2'
+    ]
+
 ];
