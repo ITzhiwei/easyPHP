f8bf7f6c4a21a4c6f734299e0f1ddf99c56393c9
ITwuzhiwei <2394599321@qq.com>
2020-02-06 00:31:24 +0800

    修改框架功能说明

--- application2/index/controller/index.php
+++ application2/index/controller/index.php
@@ -1,12 +1,12 @@
 <?php
-namespace app2\index\controller;
+namespace application2\index\controller;
 use wei\controller;
 
 class index extends controller{
     
     
     public function index(){
-        echo 'hello2';
+        echo 'application2 的 index 控制器 的 index 方法';
     }
     
     
