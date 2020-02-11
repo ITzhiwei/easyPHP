a021c74ea818cb94cbae16de88ba2c977b6ccaf6
ITwuzhiwei <2394599321@qq.com>
2020-02-12 00:48:22 +0800

    更新示例说明

--- application/index/controller/index.php
+++ application/index/controller/index.php
@@ -18,15 +18,17 @@ class index extends controller{
 
 
 
-        /* 调用其他控制器示例  Factory 第一个参数是控制器地址，第2个参数是单列管理中的别名，后面的参数是传入第一个参数的参数（即 ...index2 控制器的参数）
-        $a = Factory::get('index\index\controller\index2', 0, $this->param);
-        $a->fucName = 'hello';
-        //将用户请求结束后需要执行的内容交出来执行
-        foreach ($a->fucArr as $value){
-            $this->fucArr[] = $value;
-        }
-        $a->run();
-        */
+         /* 调用其他控制器示例  Factory 第一个参数是控制器地址，第2个参数是单列管理中的别名，后面的参数是传入第一个参数的参数（即 ...index2 控制器的参数）
+         $a = Factory::get('index\index\controller\index2', 0, $this->param);
+         $a->fucName = 'hello';
+         //将用户请求结束后需要执行的内容交出来执行,然后必须重置为[]
+         foreach ($a->fucArr as $value){
+             $this->fucArr[] = $value;
+         }
+         //重置为[]
+         $a->fucArr = [];
+         $a->run();
+         */
 
     }
     
