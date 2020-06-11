6df1b3be3b7d7412901048468e8824a4607f611d
ITwuzhiwei <2394599321@qq.com>
2020-06-12 00:28:13 +0800

    控制器首字符大写

---
+++ app3/index/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace app3\index\controller;
+use wei\controller;
+
+class Index extends controller{
+    
+    
+    public function index(){
+        echo 'app3 的 index 控制器 的 index 方法';
+    }
+    
+    
+    
+    
+}
---
+++ application2/index/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace application2\index\controller;
+use wei\controller;
+
+class Index extends controller{
+    
+    
+    public function index(){
+        echo 'application2 的 index 控制器 的 index 方法';
+    }
+    
+    
+    
+    
+}
